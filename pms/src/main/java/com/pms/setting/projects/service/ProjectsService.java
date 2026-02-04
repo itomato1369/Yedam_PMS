3953fe7c40df9d485bb44d2060e1097d745150f0
sun88110 <138852390+sun88110@users.noreply.github.com>
2026-02-04 16:38:15 +0900

    배진욱 / feat : 조회기능 추가 / 2026 - 02 - 04
    projects : entity ~ controller 패키지 및 파일 추가

--- pms/src/main/java/com/pms/setting/projects/service/ProjectsService.java
+++
@@ -1,9 +0,0 @@
-package com.pms.setting.projects.service;
-
-import java.util.List;
-
-import com.pms.setting.projects.entity.ProjectsEntity;
-
-public interface ProjectsService {
-	List<ProjectsEntity> getAllProjects();
-}
