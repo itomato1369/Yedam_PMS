92d66f47a08036afaaf47690e907c72291d24a05
S <islbvv@gmail.com>
2026-02-05 13:56:26 +0900

    chore: resources/...properties 수정
    1. application.properties : messages 추가
    2. messages..properties 생성

--- pms/src/main/java/com/pms/user/web/UserController.java
+++ pms/src/main/java/com/pms/user/web/UserController.java
@@ -1,5 +1,37 @@
 package com.pms.user.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pms.user.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
+	private final UserService userService;
+
+	@GetMapping("/newUser")
+	public String newUserForm(Model model) {
+		model.addAttribute("userDto", new UserDto());
+		return "user/registerForm";
+	}
+
+	@PostMapping("/newUser")
+	public String register(@Valid @ModelAttribute("userDto") UserDto userDto, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			return "user/registerForm";
+		}
+		return "";
+	}
 }
--- pms/src/main/java/com/pms/user/web/UserDto.java
+++ pms/src/main/java/com/pms/user/web/UserDto.java
@@ -3,10 +3,10 @@ package com.pms.user.web;
 import java.time.LocalDateTime;
 
 import com.pms.user.entity.UserEntity;
-import com.pms.user.service.password.PasswordPolicy;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,23 +16,24 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserDto {
 
-	@NotBlank(message = "ID는 필수값입니다.")
+	@NotBlank
 	private String userId;
 
-	@NotBlank(message = "PW는 필수값입니다.")
+	@NotBlank
 	private String password;
 
-	@NotBlank(message = "이름은 필수값입니다.")
+	@NotBlank
 	private String username;
 
-	@NotBlank(message = "관리자 여부는 필수값입니다.")
+	@NotNull
 	private Integer admin;
-	private Integer status;
 
-	@NotBlank(message = "이메일은 필수입니다.")
-	@Email(message = "이메일 형식이 올바르지 않습니다.")
+	private Integer status = 110;
+
+	@NotBlank
+	@Email
 	private String email;
-	
+
 	public UserEntity toEntity(String encodedPw) {
 		return UserEntity.builder()
 				.userId(this.userId.trim())
