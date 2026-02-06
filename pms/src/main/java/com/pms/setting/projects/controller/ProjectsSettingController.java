90c0bf5b4c0d0cd0b5e82640ba6f5aff25c1e4d6
sun88110 <138852390+sun88110@users.noreply.github.com>
2026-02-06 14:52:17 +0900

    refactor: JPA 하드코딩 -> 연관관계 구조변경 / 2026-02-06
    공통코드 하드코딩 구현을 연관관계 구현으로 변경

--- pms/src/main/java/com/pms/setting/projects/controller/ProjectsSettingController.java
+++ pms/src/main/java/com/pms/setting/projects/controller/ProjectsSettingController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.pms.common.ProjectStatus;
 import com.pms.setting.projects.dto.ProjectDto;
 import com.pms.setting.projects.service.ProjectsService;
 
@@ -20,29 +19,21 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/")
 public class ProjectsSettingController {
 
-    private final ProjectsService projectsService; // Service만 주입받으면 됩니다.
+    private final ProjectsService projectsService;
 
     @GetMapping("/settings")
     public String projectList(Model model) {
         model.addAttribute("projects", projectsService.getAllProjects());
-        
-        // 💡 주입받을 필요 없이 Enum 클래스에서 직접 values()를 호출합니다.
-        model.addAttribute("statusList", ProjectStatus.values()); 
-        
+        model.addAttribute("statusList", projectsService.getStatusList()); 
         return "settings"; 
     }
     
- // API 부분에서도 ProjectStatus를 주입받을 필요가 없습니다.
     @GetMapping("/api/projects/search")
     @ResponseBody
     public List<ProjectDto> searchApi(
             @RequestParam(required = false) Integer status,
             @RequestParam(required = false) String keyword
     ) {
-        if (status != null || (keyword != null && !keyword.isBlank())) {
-            return projectsService.searchProjects(status, keyword);
-        } else {
-            return projectsService.getAllProjects();
-        }
+        return projectsService.searchProjects(status, keyword);
     }
 }
