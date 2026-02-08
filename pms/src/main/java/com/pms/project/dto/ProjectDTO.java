acaebe56d532a6e6bfc3d2c9355c2e7ff3281af1
김현태 <kht082@naver.com>
2026-02-09 01:02:38 +0900

    test: 계층형 데이터 조회 및 렌더링 테스트
    불필요한 ChildProjectDTO제거
    ProjectDTO / project-mapper.xml, template\project\list.html 수정

--- pms/src/main/java/com/pms/project/dto/ProjectDTO.java
+++ pms/src/main/java/com/pms/project/dto/ProjectDTO.java
@@ -15,10 +15,14 @@ public class ProjectDTO {
     private String projectDesc;
     
     // 계층 구조용
-    private List<ChildProjectDTO> childProjects; 
+    private List<ProjectDTO> childProjects; 
     
     // 집계 데이터
     private int memberCount;       // 하위 포함 중복제거 인원
     private int totalJobCount;     // 하위 포함 중복제거 일감
+    
+    private String latestJobContent; // 최신 일감 내용
+    private String latestJobTitle;   // 최신 일감 제목
+    private Boolean hasLoginUserJoined; // 로그인 사용자가 프로젝트에 참여했는지 여부
     //private double actualProgress; // 실제 가중치 진척도
 }
