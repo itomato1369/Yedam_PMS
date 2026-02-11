14e7c89a4228873b2c88b771f7359f9a6b041d13
Dongwoo <itomato1369@gmail.com>
2026-02-11 18:05:56 +0900

    fix : 소요시간 작업에 layout 경로 문제 수정

--- pms/src/main/java/com/pms/work/controller/WorkController.java
+++ pms/src/main/java/com/pms/work/controller/WorkController.java
@@ -1,10 +1,10 @@
 package com.pms.work.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
-@Controller
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/work")
 public class WorkController {
+	private final WorkService workService;
 	// 소요시간 등록
 	@GetMapping("workinsert")
 	public String workinsert() {
