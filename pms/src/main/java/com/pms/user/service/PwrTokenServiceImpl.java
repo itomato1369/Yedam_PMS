a74ffd73e1d72c8063295adc8016d45156ac5704
S <islbvv@gmail.com>
2026-02-11 11:04:08 +0900

    featrue: mail 발송 로직 구현

--- pms/src/main/java/com/pms/user/service/PwrTokenServiceImpl.java
+++ pms/src/main/java/com/pms/user/service/PwrTokenServiceImpl.java
@@ -2,6 +2,8 @@ package com.pms.user.service;
 
 import java.util.UUID;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,26 +22,40 @@ public class PwrTokenServiceImpl implements PwrTokenService {
 	private final UserRepository userRepository;
 	private final PwrTokenRepository pwrTokenRepository;
 	private final PasswordEncoder passwordEncoder;
-	
+	private final JavaMailSender mailSender;
+
 	// TODO 반환타입 void로 변경할 것
 	// String 테스트 용
 	@Override
 	@Transactional
 	public String sendResetMail(String userId) {
 		// 존재하는 유저인지 확인
-		userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 아이디"));
+		UserEntity user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 아이디"));
 
 		// 기존에 발행된 토큰이 있는지 확인
 		// 있으면 삭제
 		pwrTokenRepository.findByUserId(userId).ifPresent(pwrTokenRepository::delete);
 
 		// 랜덤 토큰 저장
-		String tokenValue = UUID.randomUUID().toString();
-		PwrTokenEntity newToken = PwrTokenEntity.builder().userId(userId).tokenValue(tokenValue).endTime(5).build();
-
+		String tokenValue = UUID.randomUUID().toString();		
+		PwrTokenEntity newToken = PwrTokenEntity.builder()
+												.userId(userId)
+												.tokenValue(tokenValue)
+												.endTime(5)
+												.build();
+		// 메일 발송
+		sendMail(user.getEmail(), tokenValue);
 		pwrTokenRepository.save(newToken);
-		
+
 		return tokenValue;
+	}
+	
+	private void sendMail(String userMail, String tokenValue) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(userMail);
+		message.setSubject("[PMS Project] PW RESET");
+		message.setText("PW RESET LINK\n" + "http://localhost:8080/user/pwResetLink?token=" + tokenValue);
+		mailSender.send(message);
 	}
 
 	// 토큰 유효성 확인
@@ -61,7 +77,7 @@ public class PwrTokenServiceImpl implements PwrTokenService {
 
 		String encodedPw = passwordEncoder.encode(newPw);
 		user.updatePwEntity(encodedPw);
-		
+
 		// PW 변경 후 DB에서 토큰 삭제
 		pwrTokenRepository.delete(pwrToken);
 	}
