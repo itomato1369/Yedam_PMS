05e256bd8c4bf68417220c0ddb3649e230a66222
sun88110 <138852390+sun88110@users.noreply.github.com>
2026-02-11 15:14:56 +0900

    fest : 그룹 인원 표기 및 스타일 통일
    스크립트 줄수 간소화

--- pms/src/main/java/com/pms/setting/groups/vo/GroupsVO.java
+++ pms/src/main/java/com/pms/setting/groups/vo/GroupsVO.java
@@ -13,4 +13,5 @@ public class GroupsVO {
     private String groupName;  // GROUPNAME
     private Integer status;	  // 상태 코드 (510: 활성, 520: 비활성 등)
     private String statusName; // ← 조회용 필드 (DB X)
+    private Integer memberCount;
 }
