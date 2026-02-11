75121837e68ce447f943e914f1b29033671ba091
S <islbvv@gmail.com>
2026-02-11 12:18:24 +0900

    Revert "Merge pull request #5 from itomato1369/feature/create-user"
    This reverts commit da0acff78667949ba6082cfb8342bdc58e2d1ff3, reversing
    changes made to fbb6591441ae82f2b6c28f655f6e75840213356f.

--- pms/src/main/java/com/pms/userscreate/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pms.userscreate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pms.userscreate.entity.Users;
-
-public interface UsersRepository  extends JpaRepository<Users, String> {
-
-}
