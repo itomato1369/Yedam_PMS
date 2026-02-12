9c7a3f4ded2408e1b720c116197a81c2512689fe
hytkim <145666445+hytkim@users.noreply.github.com>
2026-02-12 14:46:21 +0900

    feat: 수정페이지로 가기위한 개요페이지 화면 제작

--- pms/src/main/java/com/pms/project/controller/ProjectController.java
+++ pms/src/main/java/com/pms/project/controller/ProjectController.java
@@ -88,7 +88,7 @@ public class ProjectController {
     // @PathVariable: 단일값 처리 + 매개변수에 어노테이션선언으로 필수값 선언, 반드시 받을거라 default 사용하지않기로
     @GetMapping("/{projectCode}/info")
     public String getProjectInfo() {
-    	return "index";
+    	return "project/info";
     }
     
     
