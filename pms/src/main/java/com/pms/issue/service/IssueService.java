2107a306508065b4dd6d58baae9f4dea67894546
S <islbvv@gmail.com>
2026-02-13 22:04:34 +0900

    feat: 일감 등록 시 제목 유효성 검사 추가

--- pms/src/main/java/com/pms/issue/service/IssueService.java
+++ pms/src/main/java/com/pms/issue/service/IssueService.java
@@ -3,6 +3,7 @@ package com.pms.issue.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.pms.issue.mapper.IssueMapper;
 import com.pms.issue.web.IssueDto;
@@ -23,6 +24,9 @@ public class IssueService {
 
 	// 일감 등록
 	public Integer addIssue(IssueDto issueDto) {
+		if (!StringUtils.hasText(issueDto.getTitle())) {
+			throw new RuntimeException("제목은 필수 입력 사항입니다.");
+		}
 		issueMapper.insertIssue(issueDto);
 		return issueDto.getJobNo();
 	}
