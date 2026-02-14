bd536189ad61554a67aecfe1b25cabed3e908bc2
S <islbvv@gmail.com>
2026-02-15 00:37:45 +0900

    feat: 파일 업로드 매퍼, SQL 작성 완료

--- pms/src/main/java/com/pms/issue/mapper/IssueMapper.java
+++ pms/src/main/java/com/pms/issue/mapper/IssueMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.pms.issue.web.IssueDto;
+import com.pms.issue.web.IssueFileDetailsDto;
+import com.pms.issue.web.IssueFileUploadDto;
 
 @Mapper
 public interface IssueMapper {
@@ -12,4 +14,8 @@ public interface IssueMapper {
 	List<IssueDto> selectIssue(String userId);
 
 	Integer insertIssue(IssueDto issueDto);
+
+	Integer insertIssueFile(IssueFileUploadDto fileUploadDto);
+
+	Integer insertIssueFileDetails(IssueFileDetailsDto fileDetailsDto);
 }
