f15d6568b0394bed2eadd6a7754840e799b2f170
S <islbvv@gmail.com>
2026-02-14 23:10:58 +0900

    feat: 첨부파일 업로드, 상세내용 DTO 추가

---
+++ pms/src/main/java/com/pms/issue/web/IssueFileDetailsDto.java
@@ -0,0 +1,16 @@
+package com.pms.issue.web;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+@Data
+@Alias("IssueFileDetailsDto")
+public class IssueFileDetailsDto {
+	private Integer detailsNo;
+	private Integer filesNo;
+	private String filesName;
+	private String filesSize;
+	private String filesType;
+	private String filesPath;
+}
