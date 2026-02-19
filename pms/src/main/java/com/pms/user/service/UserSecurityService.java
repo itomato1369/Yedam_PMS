9dbdf5292d0b2634d456b1562b604d3534b770e9
S <islbvv@gmail.com>
2026-02-19 16:26:02 +0900

    feat: 로그인 시 상태값 검증하는 로직 추가

--- pms/src/main/java/com/pms/user/service/UserSecurityService.java
+++ pms/src/main/java/com/pms/user/service/UserSecurityService.java
@@ -1,5 +1,6 @@
 package com.pms.user.service;
 
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,7 +22,12 @@ public class UserSecurityService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
 		UserEntity user = userRepository.findById(userId)
 				.orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 ID입니다." + userId));
-
+		
+		Integer userStatus = user.getStatus();
+		if (userStatus != 110) { 
+	        throw new DisabledException("올바른 상태 코드가 아닙니다[" + userStatus + "]. 관리자에게 문의하세요.");
+	    }
+		
 		return new CustomUserDetails(user);
 	}
 }
